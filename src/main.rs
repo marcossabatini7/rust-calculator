cc4d7be7ba9a1f20a487ce266bdfbe53c7af12af
Marcos Sabatini <marcossabatini7@gmail.com>
2022-03-19 16:39:56 -0300

    add operate function

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut args: Args = args();
 
     let first = args.nth(1).unwrap();
-    let operator = args.next().unwrap();
+    let operator = args.next().unwrap().chars().next().unwrap();
     let second = args.next().unwrap();
 
     let first_number = first.parse::<f32>().unwrap();
@@ -13,4 +13,20 @@ fn main() {
     print!("{}", first_number);
     print!("{:?}", operator);
     print!("{}", second_number);
+
+    operate(operator, first_number, second_number);
+}
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    if operator == '+' {
+        return first_number + second_number;
+    } else if operator == '-' {
+        return first_number - second_number;
+    } else if operator == '*' {
+        return first_number * second_number;
+    } else if operator == '/' {
+        return first_number / second_number;
+    } else {
+        return 0.0;
+    }
 }
