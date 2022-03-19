1477b3e2958dacf2bdb89b9ec2769f72c8548f86
Marcos Sabatini <marcossabatini7@gmail.com>
2022-03-19 16:27:45 -0300

    parse to numbers

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,11 @@ fn main() {
     let first = args.nth(1).unwrap();
     let operator = args.next().unwrap();
     let second = args.next().unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>().unwrap();
+
+    print!("{}", first_number);
+    print!("{:?}", operator);
+    print!("{}", second_number);
 }
