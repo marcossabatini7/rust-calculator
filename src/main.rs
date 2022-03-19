495bd3fc29a5ae73c97eb5899bdbfa64cc8738db
Marcos Sabatini <marcossabatini7@gmail.com>
2022-03-19 16:27:27 -0300

    getting args from console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+
+    let first = args.nth(1).unwrap();
+    let operator = args.next().unwrap();
+    let second = args.next().unwrap();
 }
