6621f6b419fe27d0c0c434194e7da3b66ff884d0
Marcos Sabatini <marcossabatini7@gmail.com>
2022-03-19 16:47:51 -0300

    create output function

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
     let result = operate(operator, first_number, second_number);
 
-    println!("Result: {}", result);
+    println!("{}", output(operator, first_number, second_number, result));
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
@@ -27,4 +27,11 @@ fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     } else {
         0.0
     }
+}
+
+fn output(operator: char, first_number: f32, second_number: f32, result: f32) -> String {
+    format!(
+        "{} {} {} = {}",
+        first_number, operator, second_number, result
+    )
 }
